c960ea582d2657957bf760be3ce2f4d7c58b0fcb
Justicesibisi <djmangsa@gmail.com>
2024-11-13 12:05:14 +0000

    Auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """Endpoint to raise a 401 Unauthorized error"""
+    abort(401)
