ac8c5f89a24d792c87422b2ac05b6d7799c16ff2
Justicesibisi <djmangsa@gmail.com>
2024-11-20 07:09:36 +0000

    task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,22 +3,30 @@
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from os import getenv
+
+from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 CORS(app)
 
-auth = BasicAuth()
+# Determine authentication type based on AUTH_TYPE
+auth = None
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "session_auth":
+    auth = SessionAuth()
+elif auth_type == "basic_auth":
+    auth = BasicAuth()
 
 @app.before_request
 def before_request():
     """Executes before every request."""
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized', '/api/v1/forbidden']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if not auth.authorization_header(request):
-        abort(401)
-    request.current_user = auth.current_user(request)
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request) and not auth.session_cookie(request):
+            abort(401)
+        request.current_user = auth.current_user(request)
 
 @app.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
