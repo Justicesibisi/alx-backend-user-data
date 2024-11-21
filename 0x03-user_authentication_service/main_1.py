8953c849954233e556d7ba77a25c619b502ad0b3
Justicesibisi <djmangsa@gmail.com>
2024-11-21 12:45:17 +0200

    Test changes

--- 0x03-user_authentication_service/main_1.py
+++ 0x03-user_authentication_service/main_1.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 """
-Main file to test add_user method in the DB class.
+Main file
 """
 
 from db import DB
+from user import User
 
 my_db = DB()
 
-# Adding the first user
 user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
 print(user_1.id)
 
-# Adding the second user
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
 print(user_2.id)
-
